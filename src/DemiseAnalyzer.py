b22bd6ccdf9b6536d27a3e1653a189bc427b0553
jlburgos <juanlb1988@gmail.com>
2012-11-20 20:07:27 -0600

    Adding initial project code. Crawler is still under construction.

---
+++ src/DemiseAnalyzer.py
@@ -0,0 +1,170 @@
+#                                   #
+#                                   #
+#   CSCE 470 Final Project          #
+#   Deadventure Time                #
+#   Created by:                     #
+#       Juan Burgos                 #
+#       Maximilian Leutermann       #
+#                                   #
+#                                   #
+from __future__ import division
+import utils, operator, re, itertools, math, random, time, nltk, pprint, string
+from nltk.stem.wordnet import WordNetLemmatizer
+from collections import defaultdict, Counter
+
+class DemiseAnalyzer(object):
+    def __init__(self):
+        lmtzr = WordNetLemmatizer()
+        # set the danger level to neutral
+        self.danger = "neutral"
+        # import negative words
+        self.negative_words = []
+        negative_words_import = set(line.strip() for line in open('negative_words.txt'))
+        for word in negative_words_import:
+            self.negative_words.append(lmtzr.lemmatize(word,'v'))
+        self.negative_words = set(self.negative_words)
+        del negative_words_import
+        # import positive words
+        self.positive_words = []
+        positive_words_import = set(line.strip() for line in open('positive_words.txt'))
+        for word in positive_words_import:
+            self.positive_words.append(lmtzr.lemmatize(word,'v'))
+        self.positive_words = set(self.positive_words)
+        del positive_words_import
+
+
+    def pre_fabricate(self, results):
+        # this is a temoprary method until we get the correct JSONs from the Google search
+        snippets = list()
+        for result in results:
+            for item in result["items"]:
+                snippets.append(item["snippet"])
+        return snippets
+
+    def onlineSearch(self,bad_words,pages,query): # TODO :: Use this once crawler is operational.
+        print "User Query = %s" % (query)
+        print "--------------------------------------------------------"
+        print "Building service client..."
+        service = build("customsearch","v1",developerKey="AIzaSyBxbY4NLqH7WWlq1Hgzcqsq29wz8d730o8")
+        print "complete!"
+        qresults = []
+        pages = min(pages,10)
+        add_words = [''] + sample(self.negative_words,bad_words)
+        print "# of randomly sampled negative terms: %d" % (bad_words)
+        print "Negative term samples:",add_words
+        print "--------------------------------------------------------"
+        print "Commencing custom search queries..."
+        for i in xrange(pages):
+          i+=1
+          for j in xrange(len(add_words)):
+            qresults.append(service.cse().list(q=query,cx='006235170055801286300:e4l76z05biw',start=i,num=10,hq=add_words[j]).execute())
+        results = []
+        for page in qresults:
+          for item in page['items']:
+            snip = item['snippet'].encode('utf-8')
+            link = item['link'].encode('utf-8')
+            self.weblinks.append(link)
+            results.append(snip)
+        print "Completed all queries, displaying snippet results below:"
+        print "--------------------------------------------------------"
+        for i in xrange(len(results)):
+          print i+1,')',results[i]
+        print "--------------------------------------------------------"
+        return results
+
+    def createResults(self, snippets):
+        # create one long string from a list of strings appending a period to the end of each string
+        str = string.join(snippets,". ")
+        # split string into sentences
+        sentences = nltk.sent_tokenize(str)
+        # create a list of all tokens in each sentence
+        sentences = [list(set(nltk.word_tokenize(sent))) for sent in sentences]
+        # tag parts of speech for each word
+        sentences = [nltk.pos_tag(sent) for sent in sentences]
+        # pull out verbs
+        grammar = r"""
+        CHUNK:
+            {<V.*>}
+            }<VBZ>{
+        """
+        cp = nltk.RegexpParser(grammar)
+        verbs = []
+        lmtzr = WordNetLemmatizer()
+        for sentence in sentences:
+            tree = cp.parse(sentence)
+            for subtree in tree.subtrees():
+                if subtree.node == 'CHUNK':
+                    # map verbs to word-net of closest root word
+                    verbs.append(lmtzr.lemmatize(subtree[0][0],'v'))
+
+        # return results in order of liklihood
+        return [pair[0] for pair in sorted(self.__sentimentDetermination__(verbs).items(), key=lambda item: item[1], reverse = True)]
+
+
+    def __sentimentDetermination__(self, verbs):
+        # count the number of times each verb occurs
+        countedVerbs = dict(Counter(verbs))
+
+        positiveVerbs = 0
+        negativeVerbs = 0
+
+        # find any words that are in the negative_words list
+        keepNegativeVerbs = set(verbs).intersection(self.negative_words)
+
+        # find any words that are in the positive_words list
+        keepPositiveVerbs = set(verbs).intersection(self.positive_words)
+
+        # delete any verbs from the counter that are not in the negative_words list
+        for word in countedVerbs.keys():
+            if word in keepPositiveVerbs:
+                positiveVerbs += countedVerbs[word]
+
+        # delete any verbs from the counter that are not in the negative_words list
+        for word in countedVerbs.keys():
+            if word not in keepNegativeVerbs:
+                del countedVerbs[word]
+
+        negativeVerbs = sum(countedVerbs.values())
+
+        # set danger level based on TF:
+        #   difference of 0 - 10 neutral
+        #   difference of 11 - 25 dangerous/safe
+        #   difference of 26< very dangerous/safe
+
+        danger = positiveVerbs - negativeVerbs
+
+        if danger > 25:
+            self.danger = "very safe"
+        elif danger > 10:
+            self.danger = "safe"
+        elif danger > -11:
+            self.danger = "neutral"
+        elif danger > -26:
+            self.danger = "dangerous"
+        else:
+            self.danger = "very dangerous"
+
+        return countedVerbs
+
+
+
+def main():
+    '''
+    users_import = set(line.strip() for line in open('mars_tweets_intermediate.json'))
+    cluster = Cluster()
+    cluster.vectorize(iter(users_import))
+    '''
+    analyzer = DemiseAnalyzer()
+    results = utils.read_results()
+    print "\n_____________________________________________________________\nWhen doing this activity you are most likely to: "
+    count = 1
+    for item in analyzer.createResults(analyzer.pre_fabricate(results)):
+        print count,item
+        count += 1
+    print "_____________________________________________________________\n"
+
+if __name__=="__main__":
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    print 'done with constructing results after %.3f seconds'%(end_time-start_time)
---
+++ test/DemiseAnalyzerTests.py
@@ -0,0 +1,31 @@
+import DemiseAnalyzer, utils, unittest
+from collections import Counter
+
+TINY_CORPUS = [
+    {"items": [{"snippet": "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Magnoliophyta, ...",}]},
+    {"items": [{"snippet": "This is a second returned result. The man drowned and died after skydiving",},]},
+    {"items": [{"snippet": "This is a third retuned search result. The man fell off the cliff and died",},]},
+    {"items": [{"snippet": "This is a fourth returned reslt. A woman named Marie fell out of an airplane, splat on the ground, and died",},]},
+    {"items": [{"kind": "customsearch#result","title": "Flower - Wikipedia, the free encyclopedia","htmlTitle": "\u003cb\u003eFlower\u003c/b\u003e - Wikipedia, the free encyclopedia","link": "http://en.wikipedia.org/wiki/Flower","displayLink": "en.wikipedia.org","snippet": "This is a fifth returned search result. The crazy kid drowned and died after falling off of a diving board and hitting his head on the ground","htmlSnippet": "A \u003cb\u003eflower\u003c/b\u003e, sometimes known as a bloom or blossom, is the reproductive structure \u003cbr\u003e  found in flowering plants (plants of the division Magnoliophyta, \u003cb\u003e... \u003c/b\u003e",},]},
+    {"items": [{"kind": "customsearch#result","title": "Flower - Wikipedia, the free encyclopedia","htmlTitle": "\u003cb\u003eFlower\u003c/b\u003e - Wikipedia, the free encyclopedia","link": "http://en.wikipedia.org/wiki/Flower","displayLink": "en.wikipedia.org","snippet": "This is a sixth returned reslt. The man hit his head, drowned and died when he fell out of the boat on the river","htmlSnippet": "A \u003cb\u003eflower\u003c/b\u003e, sometimes known as a bloom or blossom, is the reproductive structure \u003cbr\u003e  found in flowering plants (plants of the division Magnoliophyta, \u003cb\u003e... \u003c/b\u003e",},]},
+    {"items": [{"snippet": "If someone goes rock climbing they can break their arm and bleed out and ... die",},]},
+]
+
+class TestDemiseAnalyzer(unittest.TestCase):
+    def test_initialize(self):
+        # this will ensure that the user has NLTK installed and that it is culling properly
+        d = DemiseAnalyzer.DemiseAnalyzer()
+        self.assertEqual(len(d.negative_words),2388)
+        self.assertEqual(len(d.positive_words),249)
+
+    def test_create_results(self):
+        # this determines if the result generator is working correctly using the tiny corpus from above
+        d = DemiseAnalyzer.DemiseAnalyzer()
+        results = d.createResults(d.pre_fabricate(TINY_CORPUS))
+        # testing what can go wrong
+        self.assertEqual(results, ["drown","die","fall"])
+        # determining the degree of "dangerousness"
+        self.assertEqual(d.danger, "neutral")
+
+if __name__ == '__main__':
+    unittest.main()
