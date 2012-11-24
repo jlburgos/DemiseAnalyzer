76d29cbe04b5023ae08c0f43ea26a49fda817562
jlburgos <juanlb1988@gmail.com>
2012-11-23 22:30:32 -0600

    Updated DemiseAnalyzer.

--- demise_analyzer/DemiseAnalyzer.py
+++ demise_analyzer/DemiseAnalyzer.py
@@ -9,12 +9,18 @@
 #                                   #
 #                                   #
 
-from __future__ import division
-import utils, operator, re, itertools, math, random, time, nltk, pprint, string
+import utils, operator, re, itertools, math, random, time, nltk, string
+from word_semantics import word_feats
+
+import nltk.classify.util
+from nltk.classify import NaiveBayes Classifier
+from nltk.corpus import movie_reviews # For semantic analysis to be performed for NaiveBayes
 from nltk.stem.wordnet import WordNetLemmatizer
+
+from __future__ import division
 from collections import defaultdict, Counter
+
 from apiclient.discovery import build
-from random import sample
 
 class DemiseAnalyzer(object):
     def __init__(self):
@@ -35,8 +41,17 @@ class DemiseAnalyzer(object):
             self.positive_words.append(lmtzr.lemmatize(word,'v'))
         self.positive_words = set(self.positive_words)
         del positive_words_import
+        # set up structure for classifying web search results
         self.weblinks = []
+        self.trainNaiveBayes()
 
+    def trainNaiveBayes(self):
+        negids = movie_reviews.fileids('neg')
+        posids = movie_reviews.fileids('pos')
+        negfeats = [(word_feats(movie_reviews.words(fileids=[f])),'neg') for f in negids]
+        posfeats = [(word_feats(movie_reviews.words(fileids=[f])),'pos') for f in posids]
+        trainfeats = negfeats[:] + posfeats[:]
+        self.classifier = NaiveBayesCLassifier.train(trainfeats)
 
     def pre_fabricate(self, results):
         # this is a temoprary method until we get the correct JSONs from the Google search
@@ -46,7 +61,7 @@ class DemiseAnalyzer(object):
                 snippets.append(item["snippet"])
         return snippets
 
-    def onlineSearch(self,num_bad_words,num_google_pages,activity_query): # TODO :: Use this once crawler is operational.
+    def onlineSearch(self,num_bad_words,num_google_pages,activity_query):
         print "User activity_query = %s" % (activity_query)
         print "--------------------------------------------------------"
         print "Building service client..."
@@ -54,7 +69,7 @@ class DemiseAnalyzer(object):
         print "complete!"
         qresults = []
         num_google_pages = min(num_google_pages,10)
-        add_words = [''] + sample(self.negative_words,num_bad_words)
+        add_words = [''] + random.sample(self.negative_words,num_bad_words)
         print "# of randomly sampled negative terms: %d" % (num_bad_words)
         print "Negative term samples:",add_words
         print "--------------------------------------------------------"
@@ -81,6 +96,28 @@ class DemiseAnalyzer(object):
         print "--------------------------------------------------------"
         return results
 
+    def OneDimRocchio(self):
+        poscount, negcount = 0, 0
+        for url in self.weblinks:
+          # for each url, get all sentences using the web scraper.
+          # then find the sentences that contain terms in our positive and negative term lists.
+          # pull all the verbs out of these sentences for later use in determining cause of demise.
+          # run classifier on each of these sentences and accumulate results in poscount and negcount respectively.
+        danger = positiveVerbs - negativeVerbs
+
+        if danger > 25:
+            self.danger = "very safe"
+        elif danger > 10:
+            self.danger = "safe"
+        elif danger > -11:
+            self.danger = "neutral"
+        elif danger > -26:
+            self.danger = "dangerous"
+        else:
+            self.danger = "very dangerous"
+
+        return countedVerbs
+
     def createResults(self, snippets):
         # create one long string from a list of strings appending a period to the end of each string
         str = string.join(snippets,". ")
@@ -92,10 +129,10 @@ class DemiseAnalyzer(object):
         sentences = [nltk.pos_tag(sent) for sent in sentences]
         # pull out verbs
         grammar = r"""
-        CHUNK:
-            {<V.*>}
-            }<VBZ>{
-        """
+                  CHUNK:
+                  {<V.*>}
+                  }<VBZ>{
+                  """
         cp = nltk.RegexpParser(grammar)
         verbs = []
         lmtzr = WordNetLemmatizer()
@@ -110,7 +147,6 @@ class DemiseAnalyzer(object):
         return [pair[0] for pair in sorted( self.__sentimentDetermination__(verbs).items(),
                                             key=lambda item: item[1],
                                             reverse = True)]
-
 
     def __sentimentDetermination__(self, verbs):
         # count the number of times each verb occurs
@@ -157,9 +193,7 @@ class DemiseAnalyzer(object):
 
         return countedVerbs
 
-
-
-def main():
+def main(): # For testing purposes
     '''
     users_import = set(line.strip() for line in open('mars_tweets_intermediate.json'))
     cluster = Cluster()
